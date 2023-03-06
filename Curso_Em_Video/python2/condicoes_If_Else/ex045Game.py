7a6471fdbdf07cc3a0780a7706a929d4f48c2905
thiago-igor <thiago.maia0606@gmail.com>
2023-03-06 10:35:21 -0300

    Atualização

--- Curso_Em_Video/python2/condicoes_If_Else/ex045Game.py
+++ Curso_Em_Video/python2/condicoes_If_Else/ex045Game.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 
 itens = ('Pedra', 'Papel', 'Tesoura')
 computador = randint(0, 2)
@@ -7,6 +8,12 @@ print('''Suas opções:
 [1] PAPEL
 [2] TESOURA''')
 jogador = int(input('Sua Opção: '))
+
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO')
 
 print('-='*11)
 print(f'Computador jogou {itens[computador]}')
